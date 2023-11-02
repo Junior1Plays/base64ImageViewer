72df361c1f59b0a19c6f3f0e6d5b17ee61d5432c
Junior Schueller <50379079+Junior1Plays@users.noreply.github.com>
2023-11-02 03:38:55 -0300

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 const sharp = require("sharp");
 
 const app = express();
-app.use(express.json());
+app.use(express.json({ limit: '10mb' }));
 app.use(cors());
 
 app.post('/', (req, res) => {
